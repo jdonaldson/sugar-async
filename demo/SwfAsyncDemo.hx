b89473628cb8fa32930c4921921bc8be39b088a7
Justin Donaldson <donaldson@bigml.com>
2012-07-06 17:48:27 -0700

    refactoring relationship between async and promise removing threaded promise for now

--- demo/SwfAsyncDemo.hx
+++ demo/SwfAsyncDemo.hx
@@ -1,22 +1,23 @@
 import async.Promise; 
+import flash.events.IEventDispatcher;
+import flash.Lib;
 using  async.Promise;
 import flash.events.Event;
 using SwfAsyncDemo.Foo;
 import flash.events.MouseEvent;
 class SwfAsyncDemo {
-	public static function main(){
-
-			var f = flash.Lib.current.stage.clickToAsync();
-			bar.wait(f);
-	}
-	public static function bar(x:MouseEvent){
-		trace(x.localX);
-	}
+    public static function main(){
+        var f = Lib.current.stage.clickToPromise();
+        bar.wait(f);
+    }
+    public static function bar(x:MouseEvent){
+        trace(x.localX);
+    }
 }
 class Foo{
-	public static function clickToAsync(x:flash.events.IEventDispatcher):Async<MouseEvent>{
-		var y = new Async<MouseEvent>();
-		x.addEventListener(MouseEvent.CLICK,y.yield);
-		return y;
-	}
+    public static function clickToPromise(x:IEventDispatcher):Promise<MouseEvent>{
+        var y = new Promise<MouseEvent>();
+        x.addEventListener(MouseEvent.CLICK,y.yield);
+        return y;
+    }
 }
--- src/async/Promise.hx
+++ src/async/Promise.hx
@@ -2,111 +2,14 @@ package async;
 using async.Promise; 
 import haxe.macro.Context;
 import haxe.macro.Expr;
-class Promise<T>{
-    private var yield_f:Dynamic;
-    public var val(_checkval,null):T;
-    private var _val:T;
-    public var error(default,null):Promise<Dynamic>;
-    public var set(default,null):Bool;
-    private var _update:Array<T->Bool->Dynamic>;
-    private var _remove:Array<Dynamic>;
+
+
+class Promise<T> extends Async<T>{
+    public var error(default,null):Async<Dynamic>;
     public function new(){
-        set = false;
-        _update = new Array<T->Bool->Dynamic>();
-        _remove = new Array<Dynamic>();
-        error = new Promise<Dynamic>();
+        super();
+        error = new Async<Dynamic>();
     }
-    
-
-
-    /**
-     *  Yields the given value for processing on any waiting functions.
-     **/
-    public function yield(val:T){
-        trigger(val);
-        _update = new Array<Dynamic>();
-
-    }
-
-    /**
-      yields the given value for processing on any waiting functions.
-      Can be called more than once. 
-     **/
-    public function trigger(val:T){
-        set = true;
-        this._val = val;
-        for (f in _update)  {
-            yield_f = f(null,true);
-            f(this._val,null);
-        }
-        yield_f = null;
-    }
-
-
-    /**
-     *  Indicates if Promise is currently yielding for the given function.
-     **/
-    public function yieldingFor(f:Dynamic) : Bool{
-        return Reflect.compareMethods(f,this.yield_f);
-    }
-
-
-    /**
-     *  add a wait function directly to the Promise instance.
-     **/
-    public function addWait(f:T->Dynamic){
-        var f2 = function(x:T, ?ret_func:Bool) : Dynamic{
-            if (ret_func  == true) return f;
-            f(x);
-            return true;
-        }
-        _update.push(f2);
-    }
-
-    private function addUpdate(f:T->Bool->Dynamic){
-        _update.push(f);
-    }
-
-    private function linkto(a:Promise<T>){
-        var f = function(t:T, b:Bool){
-            a.yield(t);
-        }
-        this.addUpdate(f);
-    }
-
-    private function _checkval(){
-        if (!set) throw('Error: Value access on an unset Promise variable.');
-        else return _val;
-    }
-
-    /**
-     *  Removes the waited function.  This can be a single argument function given by [addWait()], 
-     *  or a multi-argument wait function given by [wait#()];
-     **/
-    public function removeWait(f:Dynamic): Bool{
-        var new_update = new Array<T->Bool->Dynamic>();
-        var found = false;
-        for (idx in 0..._update.length){
-            var rev_idx =_update.length-idx-1;
-            var original_f = cast(_update[rev_idx])(null, true);
-            if (!found && Reflect.compareMethods(original_f,f)){
-                found = true;
-                continue;
-            }
-            new_update.push(_update[rev_idx]);
-        }
-        new_update.reverse();
-        _update = new_update;
-        return found;
-    }
-
-    /**
-     *  Clears the queue of waited functions 
-     **/
-    public function clearWait(){
-        _update = new Array<T->Bool->Dynamic>();
-    }
-
     /**
      *  utility function to determine if all Promise values are set.
      **/
