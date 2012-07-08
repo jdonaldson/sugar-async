688b558f8857578655f5225c14d0f3ba4fa7e565
Justin Donaldson <donaldson@bigml.com>
2012-07-07 23:52:02 -0700

    updated demo files

--- demo/AsyncDemo.hx
+++ demo/AsyncDemo.hx
@@ -2,17 +2,17 @@ using org.sugar.Async;
 import org.sugar.Async;
 
 class AsyncDemo {
-	static var a = new Async<{x:Int}>();
+    static var a = new Async<{x:Int}>();
 
-	public static function main(){
-		var b = new Async<Int>();
-		
-	}
-	
-	public static function foo(bar:{x:Int}){
-		
-	}
-	
-	
+    public static function main(){
+        var b = new Async<Int>();
+        
+    }
+
+    public static function foo(bar:{x:Int}){
+
+    }
+
+
 }
 
--- demo/SwfAsyncDemo.hx
+++ demo/SwfAsyncDemo.hx
@@ -9,6 +9,13 @@ class SwfAsyncDemo {
     public static function main(){
         var f = Lib.current.stage.clickToPromise();
         bar.wait(f);
+        var foo1 = new Promise<Int>();
+        var foo2 = new Promise<Int>();
+        var foo3 = new Promise<Int>();
+
+        foo1.yield(1);
+        foo2.yield(1);
+        foo3.yield(1);
     }
     public static function bar(x:MouseEvent){
         trace(x.localX);
@@ -17,7 +24,7 @@ class SwfAsyncDemo {
 class Foo{
     public static function clickToPromise(x:IEventDispatcher):Promise<MouseEvent>{
         var y = new Promise<MouseEvent>();
-        x.addEventListener(MouseEvent.CLICK,y.yield);
+        x.addEventListener(MouseEvent.CLICK,y.trigger);
         return y;
     }
 }
